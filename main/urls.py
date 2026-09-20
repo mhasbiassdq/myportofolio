2d4e5f25eeda902a7669995c79ac6926a1b40f56
mhasbiassdq <muhammad.hasbi52@ui.ac.id>
2026-09-20 15:03:23 +0700

    Menambahkan URL baru untuk Experience

--- main/urls.py
+++ main/urls.py
@@ -9,9 +9,15 @@ app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
+    
     path("experience/", show_experience, name="show_experience"),
+    path("experience/add/", create_experience, name="create_experience"),
+    path("experience/<uuid:experience_id>/edit/", edit_experience, name="edit_experience"),
+    path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
+    path("api/experience/", get_experience_json, name="get_experience_json"),
+    
     path("projects/", show_projects, name="show_projects"),
     path("projects/add/", create_project, name="create_project"),
-    path("api/projects/", get_projects_json, name="get_projects_json"), # URL JSON
-    path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"), # URL Delete
+    path("api/projects/", get_projects_json, name="get_projects_json"), 
+    path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"), 
 ]
