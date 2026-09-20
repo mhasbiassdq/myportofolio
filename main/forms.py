eebdfe598dd0533c77af144ee692198a60513455
mhasbiassdq <muhammad.hasbi52@ui.ac.id>
2026-09-20 14:42:46 +0700

    feat: add form for Experience

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm, TextInput, Textarea, Select, URLInput
-from main.models import Project
+from main.models import Project, Experience 
 
 class ProjectForm(ModelForm):
     class Meta:
@@ -23,6 +23,33 @@ class ProjectForm(ModelForm):
                 "rows": 3
             }),
             "category": Select(), # Otomatis mengambil pilihan dari PROJECT_CHOICES
+            "thumbnail": URLInput(attrs={
+                "placeholder": "https://url-gambar-kamu.com"
+            }),
+        }
+        
+class ExperienceForm(ModelForm):
+    class Meta:
+        model = Experience
+        fields = ["title", "description", "category", "thumbnail"]
+        
+        labels = {
+            "title": "Posisi / Peran",
+            "description": "Deskripsi Pengalaman",
+            "category": "Kategori Pengalaman",
+            "thumbnail": "URL Logo / Gambar",
+        }
+        
+        widgets = {
+            "title": TextInput(attrs={
+                "placeholder": "Contoh: Ketua BEM / Data Science Intern", 
+                "maxlength": 255
+            }),
+            "description": Textarea(attrs={
+                "placeholder": "Ceritakan pengalaman dan tanggung jawabmu...", 
+                "rows": 3
+            }),
+            "category": Select(),
             "thumbnail": URLInput(attrs={
                 "placeholder": "https://url-gambar-kamu.com"
             }),
